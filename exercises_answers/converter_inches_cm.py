4bcb7117419599c714be78f237bbc76a2ecb3dd5
Simranjit Singh <singhz.galaxy@gmail.com>
2019-03-24 19:04:38 +0200

    Created "quiz.txt"

--- exercises_answers/converter_inches_cm.py
+++ exercises_answers/converter_inches_cm.py
@@ -1,39 +1,43 @@
-#This file runs an Inches to Centimetres converter program
+# This file runs an Inches to Centimetres converter program
 
-#Define the program that will be the converter
+# Define the program that will be the converter
+
+
 def convert():
-    #Run the program whilst checking for any errors
+    # Set the constant used for conversion
+    constant = 2.54
+
+    # Inform user about the program
+    print('Hello there! I am a tiny little program that converts a value from inches to centimetres.')
+    print(' ')
+
+    # Run the program whilst checking for any errors
     try:
-        #Set the constant used for conversion
-        constant = 2.54
+        # Ask user for a number
+        number = input('Please enter value in inches >>> ')
+        print(' ')
 
-        #Inform user about the program
-        print ('Hello there! I am a tiny little program that converts a value from inches to centimeters.')
-        print (' ')
-
-        #Ask user for a number
-        number = input ("Please enter value in inches >>> ")
-        print (' ')
-
-        #Convert string to float value
+        # Convert string to float value
         number = float(number)
 
-        #Perform the conversion and set values to be strings
+        # Perform the conversion and set values to be strings
         result = number * constant
         number = str(number)
         result = str(result)
 
-        #Save the result in a string
-        output = number + " inches is " + result + " centimeters"
+        # Save the result in a string
+        output = number + ' inches is ' + result + ' centimetres'
 
-        #Print the result
+        # Print the result
         print(output)
-        print (' ')
+        print(' ')
+        input('Press Enter to exit ')
 
-    #Run this bit of code if an error is encountered    
+    # Run this bit of code if an error is encountered
     except ValueError:
-        print("Please enter a numeric value!")
+        print('Please enter a numeric value!')
+        input('Press Enter to exit ')
 
-#Call the program to be executed when the file is run
+
+# Call the program to be executed when the file is run
 convert()
-input ('Press Enter to exit ')
